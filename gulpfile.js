e5047b320cd35246b162a23443353ef91ab53544
Weblinuxgame <15975798646@163.com>
2017-09-21 20:42:59 +0800

    config file add

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+/**
+ * create by vim
+ * @author : weblinuxgame
+ * @version 1.0.0
+ * @descrtion config for project with gulp
+ * ***/
+  const gulp 	= require('gulp');
+  const path	= require('path');
+  const config  = require('./built_env/__built/config');
+  
+  	
+  gulp.task('default',function(callback){
+  	
+	  console.log(callback);
+
+  });	
+  
