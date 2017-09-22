48dbccedabe3bc83c946163adf1b4b53fb511587
weblinuxgame <994685563@qq.com>
2017-09-22 17:28:49 +0800

    add core source code

---
+++ built_env/controllers/BaseController.js
@@ -0,0 +1,45 @@
+/**
+ * Created by weblinuxgame
+ * @class BaseController
+ * @description 基础控制
+ */
+
+class BaseController
+{
+
+    constructor(actions,modules,services)
+    {
+        this.current_action = actions ;
+        this.init();
+    }
+
+    init()
+    {
+
+    }
+
+    __proxy()
+    {
+
+    }
+
+    modules()
+    {
+
+    }
+
+    actions()
+    {
+
+    }
+
+    services()
+    {
+
+    }
+
+    hooks()
+    {
+
+    }
+}
---
+++ built_env/routers/RouterManager.js
@@ -0,0 +1,44 @@
+/**
+ * Created by weblinuxgame
+ */
+
+
+class RouterManager
+{
+    constructor(AppRouter,access_list)
+    {
+        this.instance = AppRouter || '' ;
+        this.list     = access_list || [];
+        this.init();
+    }
+
+    init()
+    {
+
+    }
+
+    middleware()
+    {
+
+    }
+
+    assets()
+    {
+
+    }
+
+    add()
+    {
+
+    }
+
+    remove()
+    {
+
+    }
+
+    error()
+    {
+
+    }
+}
